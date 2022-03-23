6cb2e8d11a853d1d80d306f9c2a902afa04b22a0
Jana VanMed <jana.vanmed@gmail.com>
2022-03-23 19:16:05 +0200

    Inittial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int value = 50;
+        double another_value = 30.2;
+
+        System.out.println("New variable value_double from value int 50 is (another_value): " + another_value);
+        double sum_value = value + another_value;
+        System.out.println(sum_value);
+
+
+
+    }
+}
